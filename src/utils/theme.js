13337760788529d3d5569b694697a069849866f5
Bartłomiej Krakowski <bk@tonik.pl>
2019-09-19 17:48:46 +0200

    feat(): Change structure of fonts variables

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,10 +2,16 @@ import {colors} from '../settings/colors';
 
 export const theme = {
   colors,
-  font: {
-    thin: 300,
-    regular: 400,
-    bold: 800,
+  fonts: {
+    typefaces: {
+      heading: 'HiraKakuStdN-W8',
+      normal: 'HiraginoSans-W6'
+    },
+    variants: {
+      thin: 300,
+      regular: 400,
+      bold: 800
+    }
   },
   media: {
     tablet: '@media (min-width: 728px)',
