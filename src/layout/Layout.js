165a2526c0cef8cf0a52f2062cb3acd4624a12a8
Bartłomiej Krakowski <bk@tonik.pl>
2019-09-19 17:24:05 +0200

    feat(): Add theme provider, layout and utilities

---
+++ src/layout/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { theme } from '../utils/theme'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`
+
+const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: ${({theme}) => theme.colors.dark};
+`
+
+const Layout = ({children}) => (
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyle/>
+      <StyledWrapper>
+        {children}
+      </StyledWrapper>
+    </>
+  </ThemeProvider>
+)
+
+export default Layout
