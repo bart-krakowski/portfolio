bfb7f18f5eb36fc8fb45f8c35428aec79b5d2c6d
Bartłomiej Krakowski <bk@tonik.pl>
2019-09-19 19:00:52 +0200

    feat(): Restructuring styled

--- src/pages/components.js
+++ src/pages/components.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '../layout/Layout';
 
-import Button from '../components/Button'
+import { StyledButton } from '../components/Button/Button.styled';
 
 const Components = () => (
   <Layout>
-    <Button text="Start a project"></Button>
+    <StyledButton>Start a project</StyledButton>
   </Layout>
 )
 
