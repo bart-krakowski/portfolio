21604b38dea2efc4ac469d830469e57dec2d6455
Bartłomiej Krakowski <bk@tonik.pl>
2019-09-24 23:52:43 +0200

    feat(): Introduce grid lines component

---
+++ src/components/GridLines/GridLines.js
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import styled, { css } from 'styled-components'
+
+const base = css`
+  width: 1px;
+  height: 100%;
+  background: radial-gradient(circle at 50% 50%, #979797, rgba(171, 171, 171, 0.59) calc(100% - 50px), rgba(200, 200, 200, 0));
+  position: absolute;
+`
+
+const LinesWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+
+  ::before, ::after {
+    ${base}
+    content: "";
+  }
+
+  ::before {
+    left: 75px;
+  }
+
+  ::after {
+    right: 75px;
+  }
+`
+const GridLine = styled.div`
+  ${base}
+  left: 50%;
+  transform: translatex(-50%);
+`
+
+export const GridLines = () => (
+  <LinesWrapper>
+    <GridLine></GridLine>
+  </LinesWrapper>
+)
