4acba6c140a9baa46a8db4fe37ac1edf5cd8a7a0
Bartłomiej Krakowski <bk@tonik.pl>
2019-10-04 20:43:23 +0200

    refactor(): Separate styles

--- src/components/ScrollIndicator/ScrollIndicator.js
+++ src/components/ScrollIndicator/ScrollIndicator.js
@@ -2,30 +2,26 @@ import React, { useRef } from "react"
 import useScrollingHandler from './scrollingHandler'
 import useScrollBar from "./useScrollBar"
 import styled from 'styled-components'
+import { Wrapper, ScrollBar } from './ScrollIndicator.styled'
 
 export default function ScrollIndicator() {
   const scrollBar = useRef(null)
   const scrollBarWrapper = useRef(null)
   useScrollingHandler(scrollBar, scrollBarWrapper)
 
-  const Wrapper = styled.div`
-    height: 150px;
-    width: 2px;
-    position: fixed;
-    bottom: 30px;
-    right: 35px;
-    background-color: ${({theme}) => theme.colors.white};
-  `
-
-  const ScrollBar = styled.div`
-    width: 100%;
-    background-color: ${({theme}) => theme.colors.background};
+  const ReadyScrollBar = styled(ScrollBar)`
     height: ${useScrollBar()}
   `
 
+/* window.addEventListener('resize', ()=> {
+  ReadyScrollBar = styled(ScrollBar)`
+    height: ${useScrollBar()}
+  `
+}) */
+
   let elements = (
     <Wrapper ref={scrollBarWrapper}>
-      <ScrollBar ref={scrollBar}/>
+      <ReadyScrollBar ref={scrollBar}/>
     </Wrapper>
   )
 
